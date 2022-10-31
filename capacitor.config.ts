f82b95a90e963c957315c36bd59fbe67f55bf35f
renzo <renzo.oostvoort@gmail.com>
2022-10-31 09:06:28 +0800

    chore: add sass

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,11 @@ const config: CapacitorConfig = {
   appId: 'com.chillflix.app',
   appName: 'chillflix',
   webDir: 'out',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server:{
+    url: 'http://192.168.18.59:4001',
+    cleartext: true
+  }
 };
 
 export default config;
